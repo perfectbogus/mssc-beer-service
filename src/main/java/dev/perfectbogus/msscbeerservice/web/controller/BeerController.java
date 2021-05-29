3b4c822c7b2c2b83213d2499605765da7491d1fb
eduardo escobar <war_army_10@hotmail.com>
2021-05-29 13:20:19 -0500

    Step 42: Beer Controller

---
+++ src/main/java/dev/perfectbogus/msscbeerservice/web/controller/BeerController.java
@@ -0,0 +1,30 @@
+package dev.perfectbogus.msscbeerservice.web.controller;
+
+import dev.perfectbogus.msscbeerservice.web.model.BeerDto;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
+
+@RequestMapping("/api/v1/beer")
+@RestController
+public class BeerController {
+
+    @GetMapping("/{beerId}")
+    public ResponseEntity<BeerDto> getBeerById(@PathVariable("beerId") UUID beerId) {
+        return new ResponseEntity<>(BeerDto.builder().build(), HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity saveNewBeer(@RequestBody BeerDto beerDto) {
+        // todo: implementation
+        return new ResponseEntity(HttpStatus.OK);
+    }
+
+    @PutMapping("/{beerId}")
+    public ResponseEntity updateBeerById(@PathVariable("beerId") UUID beerId, @RequestBody BeerDto beerDto) {
+        //todo implementation
+        return new ResponseEntity(HttpStatus.NO_CONTENT);
+    }
+}
