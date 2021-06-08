75c118bbcad208bfb19c3eb8371ae01a926caeac
eduardo escobar <war_army_10@hotmail.com>
2021-06-08 00:14:02 -0500

    Step 58: Adding BeerRepository

---
+++ src/main/java/dev/perfectbogus/msscbeerservice/repositories/BeerRepository.java
@@ -0,0 +1,9 @@
+package dev.perfectbogus.msscbeerservice.repositories;
+
+import dev.perfectbogus.msscbeerservice.domain.Beer;
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+import java.util.UUID;
+
+public interface BeerRepository extends PagingAndSortingRepository<Beer, UUID> {
+}
