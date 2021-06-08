8b2b739e61c7aabf0c1929e543a75f04b942fa4c
eduardo escobar <war_army_10@hotmail.com>
2021-06-08 18:40:55 -0500

    Step 70: error handling

--- src/main/java/dev/perfectbogus/msscbeerservice/web/controller/BeerController.java
+++ src/main/java/dev/perfectbogus/msscbeerservice/web/controller/BeerController.java
@@ -3,8 +3,10 @@ package dev.perfectbogus.msscbeerservice.web.controller;
 import dev.perfectbogus.msscbeerservice.web.model.BeerDto;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.UUID;
 
 @RequestMapping("/api/v1/beer")
@@ -13,17 +15,18 @@ public class BeerController {
 
     @GetMapping("/{beerId}")
     public ResponseEntity<BeerDto> getBeerById(@PathVariable("beerId") UUID beerId) {
+        //todo implement getBeerById
         return new ResponseEntity<>(BeerDto.builder().build(), HttpStatus.OK);
     }
 
     @PostMapping
-    public ResponseEntity saveNewBeer(@RequestBody BeerDto beerDto) {
+    public ResponseEntity saveNewBeer(@Validated @RequestBody BeerDto beerDto) {
         // todo: implementation
         return new ResponseEntity(HttpStatus.OK);
     }
 
     @PutMapping("/{beerId}")
-    public ResponseEntity updateBeerById(@PathVariable("beerId") UUID beerId, @RequestBody BeerDto beerDto) {
+    public ResponseEntity updateBeerById(@PathVariable("beerId") UUID beerId,@Validated @RequestBody BeerDto beerDto) {
         //todo implementation
         return new ResponseEntity(HttpStatus.NO_CONTENT);
     }
