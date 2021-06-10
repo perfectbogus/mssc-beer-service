063f573b96ec9b1ef1c0e4ccb9d582d8a3a0860f
eduardo escobar <war_army_10@hotmail.com>
2021-06-10 10:10:24 -0500

    Disable test cases that required to have the brewery service run on background

--- src/test/java/dev/perfectbogus/msscbeerservice/web/client/BreweryClientTest.java
+++ src/test/java/dev/perfectbogus/msscbeerservice/web/client/BreweryClientTest.java
@@ -1,6 +1,7 @@
 package dev.perfectbogus.msscbeerservice.web.client;
 
 import dev.perfectbogus.msscbeerservice.web.model.BeerDto;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -10,6 +11,7 @@ import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@Disabled
 @SpringBootTest
 class BreweryClientTest {
 
